e2b52e5badccdbd326d43b07711792496ec95e96
Guilherme Monteiro <monteiro.guilherme.andrade@gmail.com>
2025-09-23 10:51:24 -0300

    Update AtributoOrdem.cs

--- Rpg/Rpg/OrdemParanormal/AtributoOrdem.cs
+++ Rpg/Rpg/OrdemParanormal/AtributoOrdem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -8,34 +8,12 @@ using Rpg.OrdemParanormal;
 
 namespace Rpg.OrdemParanormal
 {
-
     internal class AtributoOrdem
     {
-        public Dictionary<string, int> Atributo { get; private set; }
-
-        public AtributoOrdem()
+        
+        public void AlterarAtributo(FichaCompletaOrdem ficha)
         {
-            Atributo = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase)
-            {
-                {"Agilidade", 0},
-                {"Força", 0},
-                {"Presença", 0},
-                {"Vigor", 0},
-                {"Intelecto", 0}
-            };
-        }
-        public void ExibirAtributo()
-        {
-            Console.WriteLine("----| PERICIAS |----");
-            foreach (var atributo in Atributo)
-            {
-                Console.WriteLine($"[{atributo.Value}] {atributo.Key}");
-            }
-        }
-
-        public void AlterarAtributo()
-        {
-            ExibirAtributo();
+            ficha.ExibirAtributo();
             Console.WriteLine("Coloque o Atributo que deseja alterar ");
             string AtributoSelecionado = Console.ReadLine();
 
@@ -43,21 +21,21 @@ namespace Rpg.OrdemParanormal
             int PontosSelecionados = int.Parse(Console.ReadLine());
 
 
-            Atributo[AtributoSelecionado] = PontosSelecionados;
+            ficha.Atributo[AtributoSelecionado] = PontosSelecionados;
 
             Console.Clear();
-            ExibirAtributo();
+            ficha.ExibirAtributo();
         }
 
-        public void CriandoAtributo()
-        {
+        public void CriandoAtributo(FichaCompletaOrdem ficha)
+        {            
             Console.Clear();
             int PontosAtuais = 4;
 
             while (PontosAtuais > 0)
             {
 
-                ExibirAtributo();
+                ficha.ExibirAtributo();
 
                 Console.WriteLine("Escolha um dos atributos: ");
                 string AtributoSelecionado = Console.ReadLine();
@@ -65,17 +43,17 @@ namespace Rpg.OrdemParanormal
                 Console.WriteLine($"Voce tem [{PontosAtuais}] pontos para colocar. O valor máximo inicial que você pode ter em cada atributo é 3");
                 Console.WriteLine($"Quantos pontos deseja colocar em {AtributoSelecionado}");
                 int PontoSelecionado  = int.Parse(Console.ReadLine());
-               
-                Atributo[AtributoSelecionado] = PontoSelecionado;
+
+                ficha.Atributo[AtributoSelecionado] = PontoSelecionado;
 
                 PontosAtuais = PontosAtuais - PontoSelecionado;
                 Console.Clear();
 
                 Console.WriteLine("----|Pericia Atualizadas|----");
-
+                ficha.Salvar();
             }
 
-            ExibirAtributo();
+            ficha.ExibirAtributo();
         }
 
     }
