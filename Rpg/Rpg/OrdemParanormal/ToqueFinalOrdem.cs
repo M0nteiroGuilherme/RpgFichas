c2a2866fbef889e805aca111c11426adc43784b9
Guilherme Monteiro <monteiro.guilherme.andrade@gmail.com>
2025-09-23 10:52:44 -0300

    Update ToqueFinalOrdem.cs

--- Rpg/Rpg/OrdemParanormal/ToqueFinalOrdem.cs
+++ Rpg/Rpg/OrdemParanormal/ToqueFinalOrdem.cs
@@ -1,55 +1,60 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Reflection.PortableExecutable;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Rpg.OrdemParanormal;
+
 
 namespace Rpg
 {
      internal class ToqueFinalOrdem
     {
-        public string Nome;
-        public string Aparencia;
-        public string Personalidade;
-        public string Historia;
-        public string Obejetivo;
-
-        public void Caracteristicas (){
+        public void Caracteristicas (FichaCompletaOrdem ficha)
+        {
+            
             Console.Clear();
             Console.WriteLine("\n\n| Caracteristicas do seu Personagem |--------------");
             Console.WriteLine("| ");
-            Console.WriteLine($"| Nome do seu personagem è: {Nome}");
+            Console.WriteLine($"| Nome do seu personagem è: {ficha.Personagem.Nome}");
             Console.WriteLine("| ");
-            Console.WriteLine($"| A aparencia dele é: {Aparencia}");
+            Console.WriteLine($"| A aparencia dele é: {ficha.Personagem.Aparencia}");
             Console.WriteLine("| ");
-            Console.WriteLine($"| Sua personalidade condiz com: {Personalidade}");
+            Console.WriteLine($"| Sua personalidade condiz com: {ficha.Personagem.Personalidade}");
             Console.WriteLine("| ");
-            Console.WriteLine($"| Sua historia é: {Historia}");
+            Console.WriteLine($"| Sua historia é: {ficha.Personagem.Historia}");
             Console.WriteLine("| ");
-            Console.WriteLine($"| E tem como objetivo: {Obejetivo} ");
+            Console.WriteLine($"| E tem como objetivo: {ficha.Personagem.Objetivo} ");
+
+            ficha.Salvar();
         }
 
-        public void AltaracaoCaracteristicas() {
-            ToqueFinalOrdem caracteristicas = new ToqueFinalOrdem();
+        public void AltaracaoCaracteristicas(FichaCompletaOrdem ficha) {
+
+            if (ficha.Personagem == null)
+            {
+                ficha.Personagem = new Personagem();
+            }
 
             Console.WriteLine("coloque o nome do seu personagem: ");
-            string NovoNome = Console.ReadLine();
-            caracteristicas.Nome = NovoNome;
+            ficha.Personagem.Nome = Console.ReadLine();
+            
+            Console.WriteLine($"Descreva a Aparencia do {ficha.Personagem.Nome}: ");
+            ficha.Personagem.Aparencia = Console.ReadLine();  
+            
+            Console.WriteLine($"Qual a historia do {ficha.Personagem.Nome}: ");
+            ficha.Personagem.Historia = Console.ReadLine();
+            
+            Console.WriteLine($"qual o seu obejetivo {ficha.Personagem.Nome}: ");
+            ficha.Personagem.Objetivo = Console.ReadLine();
 
-            Console.WriteLine($"Descreva a Aparencia do {NovoNome}: ");
-            string NovaAparencia = Console.ReadLine();
-            caracteristicas.Aparencia = NovaAparencia;
-
-            Console.WriteLine($"Qual a historia do {NovoNome}: ");
-            string NovaHistoria = Console.ReadLine();
-            caracteristicas.Historia = NovaHistoria;
-
-            Console.WriteLine($"qual o seu obejetivo {NovoNome}: ");
-            string NovoObjetivo = Console.ReadLine();
-            caracteristicas.Obejetivo = NovoObjetivo;
-
+            ficha.Salvar();
         }
-     }
 
+        
+     }
+     
 }
